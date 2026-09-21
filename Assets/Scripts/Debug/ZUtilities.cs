dd6799be7ecee17f0abb028353d6bfb5e544beb6
David K <davidknight11207@gmail.com>
2026-09-20 22:05:43 -0400

    Reuseable 2D lerper
    uses an "extension function" to let ZUtilities create a coroutine for linearly interpolating an object (smoothly moving from one point to another)

--- Assets/Scripts/Debug/ZUtilities.cs
+++ Assets/Scripts/Debug/ZUtilities.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 /// <summary>
-/// Static class that contains various utility functions for use elsewhere in this project.
+/// Static class that contains various utility functions and constants for use elsewhere in this project.
 /// </summary>
 public static class ZUtilities
 {
+    #region fields
+    public const float BASE_MOVEMENT_RANGE = 30f;
+    public const float DISCIPLINE_MOVEMENT_RANGE_MULTIPLIER = 0.05f;
+    #endregion fields
+
     /// <summary>
     /// Destroys all children of a GameObject. Use responsibly. Cannot lead to data loss.
     /// </summary>
@@ -38,5 +45,42 @@ public static class ZUtilities
         {
             return null;
         }
+    }
+
+    /// <summary>
+    /// Reuseable 2D lerper that attaches a coroutine to the runner monobehavior. TLDR: make sure the mono calling the coroutine does NOT get disabled until after the lerp should be done.
+    /// </summary>
+    /// <param name="runner"></param>
+    /// <param name="toMove"></param>
+    /// <param name="targetPos"></param>
+    /// <param name="lerpTime"></param>
+    public static void Generic2DLerp(this MonoBehaviour runner, GameObject toMove, Vector2 targetPos, float lerpTime)
+    {
+        runner.StartCoroutine(LerpHelper(toMove, targetPos, lerpTime));
+    }
+
+    /// <summary>
+    /// Helper function for lerper that is attached to the runner 
+    /// </summary>
+    /// <param name="moving"></param>
+    /// <param name="lerpTime"></param>
+    /// <param name="runner"></param>
+    /// <returns></returns>
+    private static IEnumerator LerpHelper(GameObject moving, Vector2 targetPos, float lerpTime)
+    {
+        float elapsed = 0f;
+        Vector2 startingPos = moving.transform.position;
+
+        while(elapsed < lerpTime)
+        {
+            elapsed += Time.deltaTime;
+            float percentage = elapsed / lerpTime;
+
+            moving.transform.position = Vector2.Lerp(startingPos, targetPos, percentage);
+
+            yield return null;
+        }
+
+        yield return null;
     }
 }
